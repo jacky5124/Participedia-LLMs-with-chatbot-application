c500e069fcf5bc1c71962728c1c7678e844b6cee
Qiye Chen <jacky5124@gmail.com>
2024-12-06 14:02:07 -0800

    Implement Plotly version of Sankey chart for Cases

--- cases_sankey/cases_sankey_plotly.py
+++ cases_sankey/cases_sankey_plotly.py
@@ -1,29 +1,122 @@
 from dash import Dash, dcc, html, Input, Output
+import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
+import pandas as pd
+from collections import Counter
 
 
-app = Dash(__name__)
+dataset = pd.read_csv('./cases_filled.csv')
 
-app.layout = html.Div([
-    html.H4('Supply chain of the energy production'),
-    dcc.Graph(id="graph"),
-])
+input_type_columns = [
+    {'label': 'Purpose', 'value': 'purposes_1'},
+    {'label': 'General Issue', 'value': 'general_issues_1'}
+]
+
+process_type_columns = [
+    {'label': 'Participant Interaction', 'value': 'participants_interactions_1'},
+    {'label': 'Decision Method', 'value': 'decision_methods_1'}
+]
+
+outcome_type_columns = [
+    {'label': 'Change', 'value': 'change_types_1'},
+    {'label': 'Implementer of Change', 'value': 'implementers_of_change_1'}
+]
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container([
+    html.Div([
+        html.H4('Input-Process-Outcome Modeling: Participation Method Effectiveness'),
+        dcc.Graph(id="graph"),
+        html.H5('Input, Process, and Outcome Types'),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    html.Label('Input Type', htmlFor='input-type'),
+                    dcc.Dropdown(
+                        id='input-type',
+                        options=input_type_columns,
+                        value=input_type_columns[0]['value'],
+                    )
+                ])),
+            dbc.Col(
+                html.Div([
+                    html.Label('Process Type', htmlFor='process-type'),
+                    dcc.Dropdown(
+                        id='process-type',
+                        options=process_type_columns,
+                        value=process_type_columns[0]['value'],
+                    )
+                ])),
+            dbc.Col(
+                html.Div([
+                    html.Label('Process', htmlFor='process'),
+                    dcc.Dropdown(id='process')
+                ])),
+            dbc.Col(
+                html.Div([
+                    html.Label('Outcome Type', htmlFor='output-type'),
+                    dcc.Dropdown(
+                        id='output-type',
+                        options=outcome_type_columns,
+                        value=outcome_type_columns[0]['value'],
+                    )
+                ]))
+        ])
+    ])
+], fluid=True)
+
+@app.callback(
+    Output('process', 'options'),
+    Input('process-type', 'value')
+)
+def set_process_options(selected_process_type):
+    options = []
+    column = dataset[selected_process_type].unique()
+    for value in column:
+        if value == 'unknown':
+            continue
+        options.append({'label': value, 'value': f"{selected_process_type};{value}"})
+    return options
+
+@app.callback(
+    Output('process', 'value'),
+    Input('process', 'options')
+)
+def set_process_value(available_options):
+    return available_options[0]['value']
 
 @app.callback(
     Output("graph", "figure"),
-    Input('dummy_input', 'value'),)
-def display_sankey(dummy_input):
-    node = dict(
-      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
-    )
+    Input('input-type', 'value'),
+    Input("process", "value"),
+    Input("output-type", "value")
+)
+def display_sankey(input_type, process, outcome_type):
+    process_type, process_value = process.split(';')
+    data = dataset.loc[dataset[process_type] == process_value, [input_type, process_type, outcome_type]]
 
-    link = dict(
-      source = [0, 1, 0, 2, 3, 3],
-      target = [2, 3, 3, 4, 4, 5],
-      value = [8, 4, 2, 8, 4, 2]
-    )
+    input_counts = Counter(data[input_type])
+    inputs = list(input_counts.keys())
+    input_indices = [i for i in range(len(inputs))]
+    input_nodes = list(input_indices)
 
-    fig = go.Figure(go.Sankey(link=link, node=node))
+    process_node = len(input_nodes)
+
+    outcome_counts = Counter(data[outcome_type])
+    outcomes = list(outcome_counts.keys())
+    outcome_indices = [i for i in range(len(outcomes))]
+    outcome_nodes = list(map(lambda i: i + process_node + 1, outcome_indices))
+
+    labels = inputs + [process_value] + outcomes
+    nodes = dict(label=labels)
+
+    sources = input_nodes + [process_node] * len(outcome_nodes)
+    targets = [process_node] * len(input_nodes) + outcome_nodes
+    values = [input_counts[i] for i in inputs] + [outcome_counts[o] for o in outcomes]
+    links = dict(source=sources, target=targets, value=values)
+
+    fig = go.Figure(go.Sankey(link=links, node=nodes))
     fig.update_layout(font_size=10)
     return fig
 
